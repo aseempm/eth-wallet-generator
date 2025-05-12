428ca86015fa4b24dfac5dd26facd68a1665bda6
Aseem PM <aseempm@protonmail.com>
2025-05-12 10:35:35 +0530

    updated the public generation for enode

--- cli.js
+++ cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const { Wallet } = require('ethers');
-const fs = require('fs');
-const readline = require('readline');
-const path = require('path');
+const { Wallet } = require("ethers");
+const fs = require("fs");
+const readline = require("readline");
+const path = require("path");
 
 // Create a readline interface
 const rl = readline.createInterface({
@@ -14,7 +14,7 @@ const rl = readline.createInterface({
 // Generate a new wallet
 const wallet = Wallet.createRandom();
 const privateKey = wallet.privateKey;
-const publicKey = wallet.publicKey;
+const publicKey = wallet.signingKey.publicKey.slice(4);
 const address = wallet.address;
 
 // Ask the user for the purpose of the wallet
@@ -28,16 +28,20 @@ rl.question("What is the purpose of creating this wallet? ", (purpose) => {
 
   // Use process.cwd() to save in the calling directory
   const fileName = `wallet_${purpose}_${Date.now()}.json`;
-  const dir = path.join(process.cwd(), 'wallets');
+  const dir = path.join(process.cwd(), "wallets");
 
   fs.mkdirSync(dir, { recursive: true });
 
-  fs.writeFile(path.join(dir, fileName), JSON.stringify(walletData, null, 2), (err) => {
-    if (err) {
-      console.error("Error saving the wallet file:", err);
-    } else {
-      console.log(`Wallet details saved to wallets/${fileName}`);
+  fs.writeFile(
+    path.join(dir, fileName),
+    JSON.stringify(walletData, null, 2),
+    (err) => {
+      if (err) {
+        console.error("Error saving the wallet file:", err);
+      } else {
+        console.log(`Wallet details saved to wallets/${fileName}`);
+      }
+      rl.close();
     }
-    rl.close();
-  });
+  );
 });
